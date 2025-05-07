a1bf06a58560b42c60f195d20547e4d9713578df
altjerry0 <altjerry0@gmail.com>
2025-05-07 20:11:49 +0100

    Github actions test for 1.0.1

--- botc-extension/src/content.js
+++
@@ -1,6 +0,0 @@
-// Content script for BotC Player Tracker
-// This script runs in the context of botc.app pages.
-
-console.log("BotC Player Tracker content script loaded.");
-
-// Future functionality for interacting with the page can be added here.
